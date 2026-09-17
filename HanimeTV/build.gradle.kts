c30bbce6136fe9047a0a4065847df83a81ef7660
antonioallomedia-blip <antonio.allomedia@gmail.com>
2026-09-18 02:59:22 +0300

    Update build.gradle.kts

--- HanimeTV/build.gradle.kts
+++ HanimeTV/build.gradle.kts
@@ -1,14 +1,9 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
 android {
     namespace = "com.yourname.hanimetv"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
@@ -26,7 +21,20 @@ tasks.withType<KotlinJvmCompile> {
     }
 }
 
-// Aucun bloc dependencies ici : le plugin CloudStream les ajoute automatiquement.
+// IMPORTANT : ces deux blocs réparent la dépendance cassée du plugin
+configurations.all {
+    resolutionStrategy.dependencySubstitution {
+        // Le plugin CloudStream essaie d'ajouter com.lagradost:cloudstream3:pre-release
+        // qui n'existe plus. On le remplace par la vraie bibliothèque.
+        substitute(module("com.lagradost:cloudstream3:pre-release"))
+            .using(module("com.github.recloudstream.cloudstream:library:-SNAPSHOT"))
+    }
+}
+
+dependencies {
+    // Ajoute la bibliothèque CloudStream manuellement
+    implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+}
 
 cloudstream {
     description = "Hanime.tv provider for CloudStream."
