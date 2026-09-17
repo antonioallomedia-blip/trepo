e0693f68c016cccd25faf8db2241176b7e5a8a4c
antonioallomedia-blip <antonio.allomedia@gmail.com>
2026-09-18 02:47:15 +0300

    Update racine build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("com.github.recloudstream.gradle:CloudstreamPlugin:v4.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
