a54120e3093c21e859ae1386a3712ece706dcc93
antonioallomedia-blip <antonio.allomedia@gmail.com>
2026-09-18 03:12:28 +0300

    Update build.gradle.kts

--- HanimeTV/build.gradle.kts
+++ HanimeTV/build.gradle.kts
@@ -18,19 +18,18 @@ android {
 tasks.withType<KotlinJvmCompile> {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_1_8)
+        // Contourne l'incompatibilité de métadonnées de kotlin-stdlib 2.4.0
+        freeCompilerArgs.add("-Xskip-metadata-version-check")
     }
 }
 
 configurations.all {
-    // 1) Force Kotlin stdlib à la version du compilateur (1.9.24).
-    //    La lib CloudStream tire stdlib 2.4.0, incompatible avec Kotlin 1.9.
     resolutionStrategy {
         force("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
         force("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.24")
         force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.24")
         force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24")
     }
-    // 2) Remplace la dépendance cassée du plugin CloudStream par la vraie.
     resolutionStrategy.dependencySubstitution {
         substitute(module("com.lagradost:cloudstream3:pre-release"))
             .using(module("com.github.recloudstream.cloudstream:library:-SNAPSHOT"))
